2ed58672fcd07ba5d6839bf870fc79f2031ba5f9
Babogaeguri <psyun0323@gmail.com>
2025-10-27 21:26:30 +0900

    PSY_update

--- aiportfolio/BL_MVO/BL_params/market_params.py
+++ aiportfolio/BL_MVO/BL_params/market_params.py
@@ -29,18 +29,32 @@ class Market_Params:
         return sigma, sectors
 
     # Pi: 내재 시장 균형 초과수익률 벡터 (N*1)
-    def making_w_mkt(self, sectors):
+    def making_w_mkt(self, sigma_sectors):
         end_month_str = self.end_date.strftime('%Y-%m')
         filtered_df = self.df[self.df['date'].dt.strftime('%Y-%m') == end_month_str].copy()
 
-        # 총 시가총액 계산
+        # 계산의 정확성을 위해 gsector가 11개인지 확인하고 인덱스로 설정
+        if len(filtered_df['gsector'].unique()) == 11:
+            filtered_df = filtered_df.set_index('gsector')
+        else:
+            raise ValueError(f"gsector의 고유값 개수가 11이 아닙니다. (현재 {len(filtered_df['gsector'].unique())}개)")
+
         total_mkt_cap = filtered_df['sector_mktcap'].sum()
         
         # 시장 가중치 계산
         w_mkt = filtered_df['sector_mktcap'] / total_mkt_cap
-
-        # sigma와 동일하게 정렬하고 섹터 반환
-        w_mkt = w_mkt.reindex(sectors).fillna(0.0)
+        
+        # 계산의 정확성을 위해 w_mkt.index와 sigma_sectors 리스트 비교
+        if (w_mkt.index == sigma_sectors).all():
+            pass
+        else:
+            raise ValueError(
+                f"w_mkt.index와 sectors가 일치하지 않습니다.\n"
+                f"w_mkt.index: {w_mkt.index}\n"
+                f"sectors: {sigma_sectors}"
+            )
+        
+        # 섹터 반환
         sectors = w_mkt.index.tolist()
 
         return w_mkt, sectors
